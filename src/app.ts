44a511c10e2b1cd2947c737a9a103cb9677f147a
Md. Rifat Sheikh <skrifat483@gmail.com>
2025-08-17 01:53:40 +0600

    project Setup completed

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+
+import express, { Request, Response } from "express";
+
+
+
+const app = express();
+
+app.get("/", (req:Request, res:Response) => {
+  res.status(200).json({
+    message : "Welcom to Tour management system backend"
+  })
+});
+export default app;
