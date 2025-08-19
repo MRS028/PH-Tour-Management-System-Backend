72e286cdc048b994d94248130e73a43257e3191b
Md. Rifat Sheikh <skrifat483@gmail.com>
2025-08-20 03:52:20 +0600

    globalerrorhandler added

--- src/app/modules/user/user.model.ts
+++ src/app/modules/user/user.model.ts
@@ -12,7 +12,7 @@ const authproviderSchema = new Schema<IAuthProvider>(
 const userSchema = new Schema<IUser>(
   {
     name: { type: String, required: true },
-    email: { type: String, required: true },
+    email: { type: String, required: true , unique:true},
     password: { type: String },
     role: {
       type: String,
--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,20 +1,21 @@
 import { IUser } from "./user.interface";
 import { User } from "./user.model";
 
+
 const createUser = async (payload: Partial<IUser>) => {
   const { name, email } = payload;
 
-  const user = await User.create({
-    name,
-    email,
-  });
-  
+  // Check if user already exists
+  const existingUser = await User.findOne({ email });
+  if (existingUser) {
+    throw new Error("User with this email already exists");
+
+  }
+
+  const user = await User.create({ name, email });
   return user;
 };
 
-
-const UserServices = {
+export const UserServices = {
   createUser,
 };
-
-export default UserServices;
