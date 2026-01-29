fd5ed042daa25920d34d75d8d5ae8ff2eeb8fe73
Md. Rifat Sheikh <skrifat483@gmail.com>
2026-01-29 23:41:26 +0600

    validation error added 29.6 okay

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,8 +1,8 @@
 import { Router } from "express";
 import { UserControllers } from "./user.controller";
 
-import { createUserSchema } from "./user.validation";
-import { validateRequest } from "../../middlewares/validateRequest";
+// import { createUserSchema } from "./user.validation";
+// import { validateRequest } from "../../middlewares/validateRequest";
 
 import { Role } from "./user.interface";
 
