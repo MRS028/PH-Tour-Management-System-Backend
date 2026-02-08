bb4365c4b8bef4db57e085c176c1b10aa10fcd61
Md. Rifat Sheikh <skrifat483@gmail.com>
2026-02-09 00:27:55 +0600

    division route added

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,20 +1,25 @@
-
 import { Router } from "express";
 import { UserRoutes } from "../modules/user/user.route";
 import { AuthRoutes } from "../modules/auth/auth.router";
+import { DivisionRoute } from "../modules/division/division.route";
+
 export const router = Router();
+
 const moduleRoutes = [
-{
-    path: '/user',
-    route: UserRoutes
-},
-{
-    path: '/auth',
-    route: AuthRoutes
-}
-]
+  {
+    path: "/user",
+    route: UserRoutes,
+  },
+  {
+    path: "/auth",
+    route: AuthRoutes,
+  },
+  {
+    path: "/division",
+    route: DivisionRoute,
+  },
+];
 
-
-moduleRoutes.forEach((route)=>{
-    router.use(route.path, route.route)
-})
+moduleRoutes.forEach((route) => {
+  router.use(route.path, route.route);
+});
