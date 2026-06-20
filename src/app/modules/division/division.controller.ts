83bb273779b4e84fea6af44c5571e53023756dfa
Md. Rifat Sheikh <skrifat483@gmail.com>
2026-06-20 07:03:58 +0600

    update Division image updated

--- src/app/modules/division/division.controller.ts
+++ src/app/modules/division/division.controller.ts
@@ -4,68 +4,77 @@ import { DivisionService } from "./division.service";
 import { sendResponse } from "../../utils/sendResponse";
 import { IDivision } from "./division.interface";
 
-const createDivision = catchAsync(async (req:Request,res: Response)=>{
-    // const result = await DivisionService.createDivision(req.body);
-    // console.log({ file: req.file, body: req.body });
+const createDivision = catchAsync(async (req: Request, res: Response) => {
+  // const result = await DivisionService.createDivision(req.body);
+  // console.log({ file: req.file, body: req.body });
 
-    const payload: IDivision = {
-        ...req.body,
-        thumbnail: req.file?.path,
-    }
-    const result = await DivisionService.createDivision(payload);
+  const payload: IDivision = {
+    ...req.body,
+    thumbnail: req.file?.path,
+  };
+  const result = await DivisionService.createDivision(payload);
 
-    sendResponse(res, {
-        statusCode: 201,
-        success: true,
-        message: "Division created successfully",
-        data: result,
-    });
+  sendResponse(res, {
+    statusCode: 201,
+    success: true,
+    message: "Division created successfully",
+    data: result,
+  });
 });
 
-const getAllDivisions = catchAsync(async (req:Request,res: Response)=>{
-    const result = await DivisionService.getAllDivisions();
-    sendResponse(res, {
-        statusCode: 200,
-        success: true,
-        message: "All divisions fetched successfully",
-        data: result,
-    });
+const getAllDivisions = catchAsync(async (req: Request, res: Response) => {
+  const query = req.query;
+  const result = await DivisionService.getAllDivisions(
+    query as Record<string, string>,
+  );
+  
+  sendResponse(res, {
+    statusCode: 200,
+    success: true,
+    message: "All divisions fetched successfully",
+    data: result,
+  });
 });
-const getSingleDivision = catchAsync(async (req:Request,res: Response)=>{
-    const slug = req.params.slug
-    const result = await DivisionService.getSingleDivision(slug);
-    sendResponse(res, {
-        statusCode: 200,
-        success: true,
-        message: "Single division fetched successfully",
-        data: result,
-    });
+const getSingleDivision = catchAsync(async (req: Request, res: Response) => {
+  const slug = req.params.slug;
+  const result = await DivisionService.getSingleDivision(slug);
+  sendResponse(res, {
+    statusCode: 200,
+    success: true,
+    message: "Single division fetched successfully",
+    data: result,
+  });
 });
-const updateDivision = catchAsync(async (req:Request,res: Response)=>{
-    const id = req.params.id;
-    const result = await DivisionService.updateDivision(id, req.body);
-    sendResponse(res, {
-        statusCode: 200,
-        success: true,
-        message: "Division updated successfully",
-        data: result,
-    });
+const updateDivision = catchAsync(async (req: Request, res: Response) => {
+  const id = req.params.id;
+  const payload: IDivision = {
+    ...req.body,
+    thumbnail: req.file?.path,
+  };
+
+  const result = await DivisionService.updateDivision(id, payload);
+  sendResponse(res, {
+    statusCode: 200,
+    success: true,
+    message: "Division updated successfully",
+    data: result,
+  });
 });
-const deleteDivision = catchAsync(async (req:Request,res: Response)=>{
-    const id = req.params.id;
-    const result = await DivisionService.deleteDivision(id);
-    sendResponse(res, {
-        statusCode: 200,
-        success: true,
-        message: "Division deleted successfully",
-        data: result,
-    });
+const deleteDivision = catchAsync(async (req: Request, res: Response) => {
+  const id = req.params.id;
+  const result = await DivisionService.deleteDivision(id);
+  sendResponse(res, {
+    statusCode: 200,
+    success: true,
+    message: "Division deleted successfully",
+    data: result,
+  });
 });
 
 export const DivisionController = {
-    createDivision,
-    getAllDivisions,
-    getSingleDivision,
-    updateDivision,
-    deleteDivision
-}
+  createDivision,
+  getAllDivisions,
+  getSingleDivision,
+  updateDivision,
+  deleteDivision,
+};
--- src/app/modules/division/division.route.ts
+++ src/app/modules/division/division.route.ts
@@ -1,27 +1,35 @@
 import { Router } from "express";
+import { z } from "zod";
 import { checkAuth } from "../../middlewares/checkAuth";
 import { Role } from "../user/user.interface";
 import { validateRequest } from "../../middlewares/validateRequest";
-import { createDivisionSchema } from "./division.validation";
+import { createDivisionSchema, updateDivisionSchema } from "./division.validation";
 import { DivisionController } from "./division.controller";
 import { multerUpload } from "../../config/multer.config";
 
 const router = Router();
 
-router.post("/create", 
-    checkAuth(Role.ADMIN,Role.SUPER_ADMIN),
-    multerUpload.single("file"),
-    validateRequest(createDivisionSchema),
-    DivisionController.createDivision
-)
+router.post(
+  "/create",
+  checkAuth(Role.ADMIN, Role.SUPER_ADMIN),
+  multerUpload.single("file"),
+  validateRequest(createDivisionSchema),
+  DivisionController.createDivision,
+);
 router.get("/", DivisionController.getAllDivisions);
 router.get("/:slug", DivisionController.getSingleDivision);
-router.patch("/:id", 
-    checkAuth(Role.ADMIN,Role.SUPER_ADMIN),
-    validateRequest(createDivisionSchema),
-    DivisionController.updateDivision
+router.patch(
+  "/:id",
+  checkAuth(Role.ADMIN, Role.SUPER_ADMIN),
+  multerUpload.single("file"),
+  validateRequest(z.object(updateDivisionSchema)),
+  DivisionController.updateDivision,
 );
 
-router.delete("/:id",checkAuth(Role.ADMIN,Role.SUPER_ADMIN), DivisionController.deleteDivision);
+router.delete(
+  "/:id",
+  checkAuth(Role.ADMIN, Role.SUPER_ADMIN),
+  DivisionController.deleteDivision,
+);
 
 export const DivisionRoute = router;
